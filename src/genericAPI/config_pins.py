f41bb540e0c4ff008ed2b21d8926cfe9c7fa6354
Guilherme Silva <guilhermelima.gui.silva@gmail.com>
2026-03-20 14:30:27 -0300

    Feat: refactoring Generic API

---
+++ src/genericAPI/config_pins.py
@@ -0,0 +1,13 @@
+# Dicionário com as definições de hardware
+VERSIONS = {
+    "bitdoglab_v06": {
+        "led_rgb": {"r": 13, "g": 11, "b": 12},
+        "buzzer": {"pin": 21},
+        "matrix": {"pin": 7, "num_leds": 25}
+    },
+    "bitdoglab_v07": {
+        "led_rgb": {"r": 13, "g": 11, "b": 12},
+        "buzzer": {"pin": 21},
+        "matrix": {"pin": 7, "num_leds": 25}
+    }
+}
